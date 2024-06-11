0c4d5df98fc0ff4c73cbf39f5d82bdabfae34301
soumyasics <soumya.sics@gmail.com>
2024-06-11 16:34:08 +0530

    msg

---
+++ server/User/userModel.js
@@ -0,0 +1,42 @@
+const mongoose= require("mongoose");
+
+const userSchema=mongoose.Schema({
+    firstname:{
+        type:String,
+       
+        required:true,
+       
+    },
+    lastname:{
+        type:String,
+      
+        required:true,
+       
+    },
+    contact:{
+        type:String,
+        
+        required:true,
+       
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true,
+       
+        dropDups: true
+    },
+    
+    password:{
+        type:String,
+        required:true
+    },
+ 
+    gender:{
+        type:String,
+        required:true
+
+    }
+});
+module.exports=mongoose.model('users',userSchema)
+
---
+++ server/routes.js
@@ -0,0 +1,17 @@
+const router=require('express').Router()
+const user=require('./User/userController')
+
+
+//user routes
+router.post('/registerUser', user.registerUser);
+router.post('/viewUserById/:id', user.viewUserById);
+router.post('/editUserById/:id', user.editUserById);
+router.post('/forgotPasswordUser', user.forgotPassword);
+router.post('/viewUsers', user.viewUsers); 
+router.post('/deleteUserById/:id', user.deleteUserById);
+router.post('/resetPasswordUser/:id', user.resetPassword);
+router.post('/loginUser', user.login);
+router.post('/requireAuthUser', user.requireAuth);
+
+
+module.exports=router
