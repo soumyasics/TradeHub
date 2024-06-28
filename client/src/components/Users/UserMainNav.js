feba7e11a7ca2b8ae3f0b4f06409a65a1c5f7b0d
Anand R P <anandrp.sics@gmail.com>
2024-06-29 00:00:44 +0530

    dashboard and user login

--- client/src/components/Users/UserMainNav.js
+++ client/src/components/Users/UserMainNav.js
@@ -1,72 +1,99 @@
-import React, { useState } from 'react'
-import './User.css'
-import { Link } from 'react-router-dom'
-import Navbar from 'react-bootstrap/Navbar';
+import React, { useState } from "react";
+import "./User.css";
+import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import logos from '../../assets/images/logo.png'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import logos from "../../assets/images/logo.png";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import { IoMdContact } from "react-icons/io";
 import { ImLoop } from "react-icons/im";
 import { BsChatText } from "react-icons/bs";
 import { IoIosSearch } from "react-icons/io";
-import { Dropdown } from 'react-bootstrap';
+import { Dropdown } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 function UserMainNav() {
   const [showDropdown, setShowDropdown] = useState(false);
-
+  const navigate = useNavigate();
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
   };
-  return (     
+
+  const handleUserLogout = () => {
+    console.log('work')
+    navigate('/user/login')
+  }
+  return (
     <div>
-      <div className='usermainnav-page-color'>
-      <Navbar collapseOnSelect expand="lg" className="" id="navfixed">
-        <div className="col-2">
-          <Navbar.Brand href="/" className="toggleimg">
-            <img src={logos} className="usermainlogoimg ms-3" alt="img"></img>
-            <span  className='usermainnav-page-trade'>trade</span> <span className='usermainnav-page-hub'>hub</span>
-          </Navbar.Brand>
-        </div>
-            <Form className="d-flex">
-                <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-5"
-                aria-label="Search"
-                />
-                
-            </Form>
-            <Nav.Link href="" className='me-5 '><p className='usermain-navbar-home mt-3 '>Home</p></Nav.Link>
-            <Nav.Link href="" className='me-5'><p className='usermain-navbar-home mt-3 '>About</p></Nav.Link>
-            <Nav.Link href="" className='me-5'><p className='usermain-navbar-home mt-3 '>My Items</p></Nav.Link>
-            <Nav.Link href="" className='me-5'><p className='usermain-navbar-chat mt-3 pt-1'><BsChatText/> Chat</p></Nav.Link>
-            <Nav.Link href="" className='me-5'><p className='usermain-navbar-chat mt-3 pt-1'>+Sell</p></Nav.Link>
-            <Nav.Link href="" className='me-5'><p className='usermain-navbar-chat mt-3 pt-1'>Points</p></Nav.Link>
-            <Nav.Link href="" className='me-5'><ImLoop className='usermain-navbar-iconloop'/></Nav.Link>
-            <Dropdown show={showDropdown} onToggle={toggleDropdown}>
-              <Dropdown.Toggle  as="div" onClick={toggleDropdown} className='custom-dropdown-toggle'>
-                <Nav.Link href="" className='me-5 '><IoMdContact className='usermain-navbar-iconloop mt-3'/></Nav.Link>
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Link class="dropdown-item" to="/user/user-profile" id="">
-                  View Profile
-                </Link>
-                <Link class="dropdown-item" to="" id="">
-                  Whishlist
-                </Link>
-                <Link class="dropdown-item" to="" id="">
-                  My Orders
-                </Link>
-                <Link class="dropdown-item" to="" id="">
-                  Logout
-                </Link>
-              </Dropdown.Menu>
-            </Dropdown>
-      </Navbar>
+      <div className="usermainnav-page-color">
+        <Navbar collapseOnSelect expand="lg" className="d-flex justify-content-between pe-5" id="navfixed">
+          <div className="col-2">
+            <Navbar.Brand href="/" className="toggleimg">
+              <img src={logos} className="usermainlogoimg ms-3" alt="img"></img>
+              <span className="usermainnav-page-trade">trade</span>{" "}
+              <span className="usermainnav-page-hub">hub</span>
+            </Navbar.Brand>
+          </div>
+          {/* <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-5"
+              aria-label="Search"
+            />
+          </Form> */}
+          
+          <Nav.Link href="" className="me-5 ">
+            <p className="usermain-navbar-home mt-3 ">Home</p>
+          </Nav.Link>
+          <Nav.Link href="" className="me-5">
+            <p className="usermain-navbar-home mt-3 ">About</p>
+          </Nav.Link>
+          {/* <Nav.Link href="" className='me-5'><p className='usermain-navbar-home mt-3 '>My Items</p></Nav.Link> */}
+          {/* <Nav.Link href="" className="me-5">
+            <p className="usermain-navbar-chat mt-3 pt-1">
+              <BsChatText /> Chat
+            </p>
+          </Nav.Link> */}
+          <Nav.Link href="" className="me-5">
+            <p className="usermain-navbar-chat mt-3 pt-1">+Sell</p>
+          </Nav.Link>
+          <Nav.Link href="" className="me-5">
+            <p className="usermain-navbar-chat mt-3 pt-1">Points</p>
+          </Nav.Link>
+          {/* <Nav.Link href="" className="me-5">
+            <ImLoop className="usermain-navbar-iconloop" />
+          </Nav.Link> */}
+          <Dropdown show={showDropdown} onToggle={toggleDropdown}>
+            <Dropdown.Toggle
+              as="div"
+              onClick={toggleDropdown}
+              className="custom-dropdown-toggle"
+            >
+              <Nav.Link href="" className="me-5 ">
+                <IoMdContact className="usermain-navbar-iconloop mt-3" />
+              </Nav.Link>
+            </Dropdown.Toggle>
+            <Dropdown.Menu id="drop-down-basic">
+              <Link className="dropdown-item" to="/user/user-profile" id="">
+                View Profile
+              </Link>
+              <Link className="dropdown-item" to="" id="">
+                Whishlist
+              </Link>
+              <Link className="dropdown-item" to="" id="">
+                My Orders
+              </Link>
+              <h6 className="dropdown-item bg-danger text-light" to="" id="" onClick={handleUserLogout} >
+                Logout
+              </h6>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar>
       </div>
     </div>
-  )
+  );
 }
 
-export default UserMainNav
+export default UserMainNav;
--- client/src/components/Users/Userlogin.js
+++ client/src/components/Users/Userlogin.js
@@ -1,141 +1,155 @@
-import React, { useState } from 'react'
-import './User.css';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import adminlogin from '../../assets/images/adminlogin.png'
-import { Link, useNavigate } from 'react-router-dom';
-import MainNav from '../homeComponents/Navbar/MainNav';
-import Footer from '../Footer/Footer';
-import axiosInstance from '../../apis/axiosInstance';
+import React, { useState } from "react";
+import "./User.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import adminlogin from "../../assets/images/adminlogin.png";
+import { Link, useNavigate } from "react-router-dom";
+import MainNav from "../homeComponents/Navbar/MainNav";
+import Footer from "../Footer/Footer";
+import axiosInstance from "../../apis/axiosInstance";
 function Userlogin() {
-
-  const[data,setData]=useState({
-    email:"",
-    password:""
+  const [data, setData] = useState({
+    email: "",
+    password: "",
   });
 
-  const[errors,setErrors]=useState({
-    email:"",
-    password:""
-  })
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
 
-  const[formIsValid,setFormIsValid]=useState(true);
+  const [formIsValid, setFormIsValid] = useState(true);
 
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
-  const handleChange = (e) =>{
-    const {name,value} = e.target;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
     setData({
-      ...data,[name]:value,
+      ...data,
+      [name]: value,
     });
-    setErrors((prevErrors) => ({...prevErrors,[name]:""}));
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
   };
 
   const validateField = (fieldName, value) => {
     if (!value.trim()) {
-        setFormIsValid(false);
-        return `${fieldName} is required`;
+      setFormIsValid(false);
+      return `${fieldName} is required`;
     }
-    return '';
-};
+    return "";
+  };
 
-  const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
     let errors = {};
     let formIsValid = true;
 
-    errors.email = validateField('Email', data.email);
+    errors.email = validateField("Email", data.email);
     if (errors.email) formIsValid = false;
 
-    errors.password = validateField('Password', data.password);
+    errors.password = validateField("Password", data.password);
     if (errors.password) formIsValid = false;
 
     setErrors(errors);
     setFormIsValid(formIsValid);
-    
-    if(formIsValid){
-      axiosInstance.post('/loginUser',data)
-      .then((res)=>{
-        if(res.data.status === 200){
-          console.log("Login Successfully",res);
-          alert("Login Successfully")
-          localStorage.setItem('userId',res.data.data._id)
-          navigate('/user/home')
-        }
-        else{
-          alert("Error")
-          console.log("error");
-        }
-      })
-      .catch((err)=>{
-        console.log("Error",err);
-        alert("Error")
-      })
+
+    if (formIsValid) {
+      axiosInstance
+        .post("/loginUser", data)
+        .then((res) => {
+          if (res.data.status === 200) {
+            console.log("Login Successfully", res);
+            alert("Login Successfully");
+            localStorage.setItem("userId", res.data.data._id);
+            navigate("/user/home");
+          } else {
+            alert("Error");
+            console.log("error");
+          }
+        })
+        .catch((err) => {
+          console.log("Error", err);
+          alert("Error");
+        });
     }
+  };
 
-
-  }
-
-  return (   
+  return (
     <div>
-      <MainNav/>
+      <MainNav />
       <div>
-      <div className='container user-login-box mb-5 mt-5'>
-        <div className=' user-login-box1'>
+        <div className="container user-login-box mb-5 mt-5">
+          <div className=" user-login-box1">
             <Row>
-              <Col className='container'>
-                <img className="user-login-img" src={adminlogin} alt='img'></img>
+              <Col className="container">
+                <img
+                  className="user-login-img"
+                  src={adminlogin}
+                  alt="img"
+                ></img>
               </Col>
               <Col>
-                <h2 className='user-login-h2'>User Login</h2>
+                <h2 className="user-login-h2">User Login</h2>
                 <form onSubmit={handleSubmit}>
                   <div>
-                    <label className='user-login mt-5 ms-5'>Email</label>
-                    <input  
-                    className="user-login-textbox ms-5 ps-3" 
-                    placeholder='Email'
-                    type='email'
-                    name='email'
-                    value={data.email}
-                    onChange={handleChange}
+                    <label className="user-login mt-5 ms-5">Email</label>
+                    <input
+                      className="user-login-textbox ms-5 ps-3"
+                      placeholder="Email"
+                      type="email"
+                      name="email"
+                      value={data.email}
+                      onChange={handleChange}
                     ></input>
-                    {errors.email && <div className='user-login-error mt-2  text-danger'>{errors.email}</div>}
+                    {errors.email && (
+                      <div className="user-login-error mt-2  text-danger">
+                        {errors.email}
+                      </div>
+                    )}
                   </div>
                   <div>
-                    <label className='user-login mt-5 ms-5'>Password</label>
-                    <input  
-                    className="user-login-textbox ms-2 ps-3" 
-                    placeholder='Password'
-                    type='password'
-                    name='password'
-                    value={data.password}
-                    onChange={handleChange}
+                    <label className="user-login mt-5 ms-5">Password</label>
+                    <input
+                      className="user-login-textbox ms-2 ps-3"
+                      placeholder="Password"
+                      type="password"
+                      name="password"
+                      value={data.password}
+                      onChange={handleChange}
                     ></input>
-                    {errors.password && <div className='user-login-error mt-2 text-danger'>{errors.password}</div>}
+                    {errors.password && (
+                      <div className="user-login-error mt-2 text-danger">
+                        {errors.password}
+                      </div>
+                    )}
                   </div>
-                  <div className='mt-3'>
-                    <Link to="/user/forgetpswd" className='user-login-forget'>Forget Password?</Link>
+                  <div className="mt-3">
+                    <Link to="/user/forgetpswd" className="user-login-forget">
+                      Forget Password?
+                    </Link>
                   </div>
-                  <div className='text-center'>
-                    <button type='submit' className='user-login-btn mt-5' >Login</button>
+                  <div className="text-center">
+                    <button type="submit" className="user-login-btn mt-5">
+                      Login
+                    </button>
                   </div>
                   <div className="mt-4 ms-5">
-                        <h6 className="text-center">
-                       New to TradeHub?{" "}
-                        <Link to="/user/register" className="">
-                            Register Now
-                        </Link>
+                    <h6 className="text-center">
+                      New to TradeHub?{" "}
+                      <Link to="/user/register" className="">
+                        Register Now
+                      </Link>
                     </h6>
                   </div>
                 </form>
-              </Col> 
+              </Col>
             </Row>
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
-    <Footer/>
-    </div>
-  )
+  );
 }
 
-export default Userlogin
+export default Userlogin;
