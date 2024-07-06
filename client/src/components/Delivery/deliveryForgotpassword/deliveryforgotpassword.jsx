c23e356cbca0484be641dfb85471ecd6767414a9
manu006m <manu006m@gmail.com>
2024-07-06 14:18:03 +0530

    forget password

--- client/src/components/Delivery/deliveryForgotpassword/deliveryforgotpassword.jsx
+++ client/src/components/Delivery/deliveryForgotpassword/deliveryforgotpassword.jsx
@@ -1,22 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./deliveryforgotpassword.css";
 import img1 from "../../../assets/images/passwordImg.png";
 import MainNav from "../../homeComponents/Navbar/MainNav";
 import Footer from "../../Footer/Footer";
+import { useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 
 function Deliveryforgotpassword() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmpassword] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      toast.error("Please enter a valid email address");
+      return false;
+    }
+
+    if (!password) {
+      toast.error("password is required");
+      return;
+    }
+    if (password.length < 8) {
+      toast("Password needs minimum 8 characters");
+      return;
+    }
+    if (!confirmPassword) {
+      toast.error("confirm password is required");
+      return;
+    }
+    if (password !== confirmPassword) {
+      alert.error("password doesn't matches");
+      return;
+    }
+    sendToServer({email})
+  };
+
   return (
     <div>
       <div>
-        <MainNav/>
+        <MainNav />
       </div>
       <div className="mb-5 container">
         <div className="deliveryagent-forgot-box ">
           <div className="row">
             <div className="col">
-              <img src={img1} alt="img" className="deliveryagent-forgot-img"></img>
+              <img
+                src={img1}
+                alt="img"
+                className="deliveryagent-forgot-img"
+              ></img>
             </div>
-            <form className="col">
+            <form className="col" onSubmit={handleSubmit}>
               <div className="container mt-5 text-center">
                 <h2>Forget Password</h2>
               </div>
@@ -28,6 +65,9 @@ function Deliveryforgotpassword() {
                       className="deliveryagent-forgot-textbox mt-5"
                       placeholder="Enter email"
                       name="email"
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
                     />
                   </div>
                   <div>
@@ -36,6 +76,9 @@ function Deliveryforgotpassword() {
                       className="deliveryagent-forgot-textbox mt-5"
                       placeholder="Enter new Password"
                       name="password"
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
                     />
                   </div>
                 </div>
@@ -45,6 +88,9 @@ function Deliveryforgotpassword() {
                     className="deliveryagent-forgot-textbox mt-5"
                     placeholder="Re-Enter new Password"
                     name="confirmpassword"
+                    onChange={(e) => {
+                      setConfirmpassword(e.target.value);
+                    }}
                   />
                 </div>
               </div>
@@ -56,10 +102,9 @@ function Deliveryforgotpassword() {
             </form>
           </div>
         </div>
-       
       </div>
       <div>
-        <Footer/>
+        <Footer />
       </div>
     </div>
   );
