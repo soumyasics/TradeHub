6eab2dde5abe480adb1259161367b2885f7c0e30
manu006m <manu006m@gmail.com>
2024-07-10 09:57:35 +0530

    manu

--- client/src/components/Users/userEditProfileCard/userEditProfileCard.jsx
+++ client/src/components/Users/userEditProfileCard/userEditProfileCard.jsx
@@ -1,31 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./userEditProfileCard.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import { FaArrowLeft } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import axiosInstance from "../../../apis/axiosInstance";
 
 export const UsereditProfileCard = () => {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
-  const handleShow = () => 
-  {
+  const [data,setData] =useState(
+    {
+      firstName:"",
+      lastName:"",
+      email:"",
+      phoneNumber:""
+    }
+  )
+  const handleShow = (id) => {
     setShow(true);
-   
+  };
+const handleChange = (e) =>
+  {
+    const {name,value} = e.target;
+    setData({...data,[name]:value})
+    console.log(data);
   }
-  
   return (
     <div>
-      <Button variant="primary" onClick={handleShow} className="userEditProfile-base-button" >
+      <Button
+        variant="primary"
+        onClick={handleShow}
+        className="userEditProfile-base-button"
+      >
         Edit
       </Button>
       <div className="editProfile-card-body">
         <Modal show={show} onHide={handleClose}>
           <Modal.Header className="userEditProfileCard-header">
             <Modal.Title className="EditProfileCard-heading d-flex">
-              <FaArrowLeft className="userEditProfile-left-arrow" onClick={handleClose} />
+              <FaArrowLeft
+                className="userEditProfile-left-arrow"
+                onClick={handleClose}
+              />
               <p> Edit profile</p>
             </Modal.Title>
           </Modal.Header>
@@ -42,6 +60,9 @@ export const UsereditProfileCard = () => {
                   placeholder="Enter your name"
                   autoFocus
                   className="editProfileCard-input"
+                  name="firstName"
+                  onChange={handleChange}
+                  value={data.firstName}
                 />
               </Form.Group>
               <Form.Group
@@ -56,6 +77,10 @@ export const UsereditProfileCard = () => {
                   placeholder="Enter your name"
                   autoFocus
                   className="editProfileCard-input"
+                  name="lastName"
+                  onChange={handleChange}
+                  value={data.lastName}
+
                 />
               </Form.Group>
 
@@ -64,7 +89,15 @@ export const UsereditProfileCard = () => {
                 controlId="exampleForm.ControlTextarea1"
               >
                 <Form.Label className="editProfileCard-Label">Email</Form.Label>
-                <Form.Control type="email" className="editProfileCard-input" />
+                <Form.Control
+                  type="email"
+                  className="editProfileCard-input"
+                  placeholder="Enter your email"
+                  name="email"
+                  onChange={handleChange}
+                  value={data.email}
+
+                />
               </Form.Group>
 
               <Form.Group
@@ -74,7 +107,15 @@ export const UsereditProfileCard = () => {
                 <Form.Label className="editProfileCard-Label">
                   Phone Number
                 </Form.Label>
-                <Form.Control type="number" className="editProfileCard-input" />
+                <Form.Control
+                  type="number"
+                  className="editProfileCard-input"
+                  placeholder="Enter your phone number"
+                  name="phoneNumber"
+                  onChange={handleChange}
+                  value={data.phoneNumber}
+
+                />
               </Form.Group>
             </Form>
           </Modal.Body>
