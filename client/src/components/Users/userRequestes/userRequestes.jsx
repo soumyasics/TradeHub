0f95e5292aa1bcbd7f9537e78fd7f9defeda507c
Anand R P <anandrp.sics@gmail.com>
2024-07-14 18:51:30 +0530

    received and sent request

--- client/src/components/Users/userRequestes/userRequestes.jsx
+++ client/src/components/Users/userRequestes/userRequestes.jsx
@@ -1,5 +1,7 @@
 import Footer from "../../Footer/Footer";
 import UserMainNav from "../UserMainNav";
+import { ReceivedRequest } from "./receivedRequest";
+import { RequestedItems } from "./requestedItems";
 import { RequestsToggle } from "./requestsToggle";
 import { useState } from "react";
 export const UserRequestes = () => {
@@ -13,11 +15,7 @@ export const UserRequestes = () => {
         setReceivedRequest={setReceivedRequest}
       />
       <div style={{ minHeight: "600px" }}>
-        {receivedRequest ? (
-          <div>received request</div>
-        ) : (
-          <div>request sent</div>
-        )}
+        {receivedRequest ? <ReceivedRequest /> : <RequestedItems />}
       </div>
       <Footer />
     </div>
